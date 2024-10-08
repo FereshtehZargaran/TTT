99f2ecd51772376128c1ff31c0fc7efcfba7d7c7
FereshtehZargaran <fereshteh.zahra@ns.nl>
2024-10-08 21:41:39 +0200

    Initializing the board with empty state.
    The test is passed.

--- TTT/TTT/TicTacToeViewModel.swift
+++ TTT/TTT/TicTacToeViewModel.swift
@@ -4,4 +4,12 @@ final class TicTacToeViewModel: ObservableObject {
 
     @Published var board = [[Tile]]()
 
+    init() {
+        resetBoard()
+    }
+
+    func resetBoard() {
+        board = Array(repeating: Array(repeating: Tile(type: .Empty), count: 3), count: 3)
+    }
+
 }
