0dba21dfc5e0351ab7eac73b26b9d30d15327944
FereshtehZargaran <fereshteh.zahra@ns.nl>
2024-10-08 21:11:42 +0200

    Creating a zero state.

---
+++ TTT/TTT/TTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTTApp.swift
+//  TTT
+//
+//  Created by Fereshteh Zargaran on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
